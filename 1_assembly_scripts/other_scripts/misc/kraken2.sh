035e76cb7bdb08ce05e12f20eb0120bbefcf64b3
Oakem Kyne <oakemkyne@gmail.com>
2024-05-13 07:10:13 +0100

    bakta annotation stuff

---
+++ 1_assembly_scripts/other_scripts/misc/kraken2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH -A moru-batty.prj
+#SBATCH -J kraken2
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
+#SBATCH --cpus-per-task 6
+#SBATCH --mem-per-cpu=15G
+#SBATCH -p short
+
+echo started=`date`
+echo "job=$SLURM_JOB_ID"
+echo "hostname="`hostname`
+echo "OS="`uname -s`
+echo "username="`whoami`
+
+# This code takes a single fastq.gz file and runs kraken2 on it, the specific db needs to be specified
+
+rootdir="/well/moru-batty/projects/ot"
+#db="/well/moru-batty/projects/krakendb/standard-plus-mouse"
+db="/well/moru-batty/projects/krakendb/standard"
+sample="sample"
+
+# Check if the input FASTQ file is provided as a command-line argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <fastqfile>"
+    echo "Please specify the input FASTQ file."
+    exit 1
+fi
+
+# Get the input FASTQ file from the command-line argument
+fastqfile="$1"
+
+# Extract the base filename (without path and extension)
+input_filename=$(basename "$fastqfile" .fastq.gz)
+
+# Set the sample name to the input filename
+sample="$input_filename"
+
+kraken2 --db ${db} --threads 6 --output ${sample}.out --report ${sample}.report --gzip-compressed ${fastqfile}
+
+echo "finished="`date`
+exit 0
+
---
+++ 2_annotation_scripts/1_bakta_annotation/1_bakta_script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH -A moru-batty.prj
+#SBATCH -J bakta
+#SBATCH --output=bakta_%j.out
+#SBATCH --error=bakta_%j.err
+#SBATCH --cpus-per-task 8
+#SBATCH --mem-per-cpu=15G
+#SBATCH -p short
+
+echo started=`date`
+echo "job=$SLURM_JOB_ID"
+echo "hostname="`hostname`
+echo "OS="`uname -s`
+echo "username="`whoami`
+
+# Activate bakta environment
+source /well/moru-batty/users/vhs789/miniforge/etc/profile.d/conda.sh
+conda activate bakta
+
+# May want to modify the code so the protein identified based on the location of the script?
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <prefix> <jeanne_proteins> <genome>"
+    exit 1
+fi
+
+# Prefix for naming the files and also used in the locus tag
+prefix="$1"
+# Trusted proteins which we are testing/adding
+jeanne_proteins="$2"
+# The genome to be annotated
+genome="$3"
+# Path to bakta database
+database="/well/moru-batty/projects/ot/annotation/bakta_db/db"
+
+bakta --db "$database" --prefix "$prefix" --complete --proteins "$jeanne_proteins" --gram - --locus-tag "$prefix" --threads 8 --output "$prefix" "$genome"
+
+echo "Script finished"
+exit 0
