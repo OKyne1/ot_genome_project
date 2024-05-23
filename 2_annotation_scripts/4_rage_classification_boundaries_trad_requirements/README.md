ffc7f162762252fa39fc17e631a78a99732938bb
OKyne1 <oakemkyne@gmail.com>
2024-05-23 10:22:03 +0700

    classsifcation stuff (largely)

---
+++ 2_annotation_scripts/4_rage_classification_boundaries_trad_requirements/README.md
@@ -0,0 +1,73 @@
+# Annotation of RAGE regions
+## Code structure 
+1. Defining RAGE derived regions
+2. Defining boundaries for complete RAGEs
+3. Validating boundaries within RAGE derived regions
+4. Identifying complete RAGEs based on required genes
+
+![workflow](./diagrams/code_workflow.png)
+
+These stages are tied together in the script `main.sh` so that processing of genbank files can be performed in one stage to define the RAGEs derived regions and complete RAGEs. Output files .bed files.
+
+### Code usage
+1. Create and activate the rage conda environment. This can be created from `rage_environment.yml`.
+2. The script can be run like this (locally, not adjusted for the cluster):  `bash ../main.sh <gbff_file> <gbff_file> ... <gbff_file>`. 
+
+## RAGE derived regions
+These are currently defined by a list of different rage genes. These were determined by identifying genes in the bakta annotation which were considered in RAGE regions by Jeanne's paper. 
+
+This list of RAGE genes was generated from the Karp, Gilliam and Boryong genomes, then tested on other genomes to validate the success.
+
+### Visualising success
+![kato rage derived regions](./diagrams/kato_rage_derived.png)
+This IGV image of the different boundaries shows that my script closely resembles the manual annotations (note manual annotations have additional breaks for boundaries between RAGEs). The 2 lines below the manual annotated boundaries shows the effects of allowing one or 2 mis-matched genes within a RAGE region.
+
+The effect here is not that large and so the current script only allows 1 non-match. However it can easily be modified to permit 2. The code was developed to permit this, and also not to allow these non-matches to be adjacent to eachother. 
+
+### Rules for the RAGE derived regions
+1. Needs to be more than 2 genes next to eachother
+2. A region can contain a single non-matched gene (but not at the edges)
+3. Genes must match the `RAGE_gene_list` but they cannot match to the `exclusion_list` (this was generated so that genes which match the list but contain more information and should be excluded)
+
+
+## Complete RAGE boundaries
+### Rules for boundaries are:
+<img src="https://github.com/OKyne1/ot_genome_project/blob/main/2_annotation_scripts/4_rage_classification/diagrams/rage_boundaries_conditions.png" width="500">
+The black lines mark the boundary, if a gene is not included in the boundary then the script will restart on this line to identify additional boundaries using this gene.
+
+### Edge case:
+<img src="https://github.com/OKyne1/ot_genome_project/blob/main/2_annotation_scripts/4_rage_classification/diagrams/edge_case.png" width="800">
+
+The script currently allows 2 RAGE regions to share a dnaA gene. This is unlikely to cause any issues as the chance of 2 complete RAGEs next to eachother is extremely unlikely and not problematic.
+
+
+## Validating boundaries
+Boundaries identified are validated against the RAGE derived regions. Only those contained within this .bed file are kept for required gene testing. This step uses the package bed tools.
+
+
+## Testing for required genes
+To be a complete RAGE, it must have the boundries (already identified), all tra genes, at least 1 transposase and at least 1 cargo gene (currently using those defined in the paper). The presence of these genes is tested on the validated boundaries and the regions which meet these criteria are outputted in a new .bed file.
+
+
+## Initial results
+These scripts identify **100%** of complete RAGEs (3/3) but it also identifies **17/52** of the complete RAGEs with truncated genes. This is probably better than underclassification, however, we need to find a way to identify which of these are not truncated.
+
+
+## RAGE gene completeness
+Overclassification of RAGEs, is a result of truncated or degraded genes not being identified. Using a Blast+ script 
+
+Method:
+1. Blast+ search of the genomes genes against a database of complete tra genes and complete integrases
+2. Testing the percentage of the genome aligned
+
+This was successful in identifying >95% of all cases (when testing 7 genomes using a db of the 8th), consequently the coverage will be increased when the all 8 are used in the db.
+
+## Handling Contigs
+I modified the script to handle contigs by spliting the input txt files and then joining the outputs. 
+![kato rage derived regions](./diagrams/contig_vs_complete_rage_derived2.png)
+Note: this diagram is illustrative, but as the bed files here aren't perfect (adapted contig values to the reference genome imperfectly and unfortunately the rage regions bed for comparison is from an earlier itteration of the script).
+
+
+## Current limitations
+- Bed files are currently indexed wrong. Need to -1 from all start entries. In reality this makes very little difference, hence hasn't yet been resolved.
+- Contig naming in bakta is different from the naming in the fasta file (input), this difference is continued into the bed files.
---
+++ 2_annotation_scripts/4_rage_classification_boundaries_trad_requirements/scripts/3_boundaries_to_complete.py
@@ -0,0 +1,124 @@
+import argparse
+import os
+
+# Inputs: .bed and xxx.txt
+# Outputs: xxx_complete_RAGEs.bed
+
+def parse_bed_file(bed_file):
+    bed_dict = {}
+    with open(bed_file, 'r') as bed:
+        for line in bed:
+            fields = line.strip().split('\t')
+            bed_line = line.strip()  # Entire line as key
+            region_id = fields[0]  # Assuming the first field is a unique identifier
+            bed_dict[bed_line] = (region_id, int(fields[1]), int(fields[2]))
+    return bed_dict
+
+def parse_txt_file(txt_file, bed_dict):
+    result_dict = {}
+    with open(txt_file, 'r') as txt:
+        for line in txt:
+            fields = line.strip().split('\t')
+            start_txt = int(fields[3])
+            end_txt = int(fields[4])
+            for bed_line, (region_id, start_bed, end_bed) in bed_dict.items():
+                if start_bed <= start_txt <= end_bed and start_bed <= end_txt <= end_bed:
+                    region = (fields[0], fields[1], fields[2], fields[3], fields[4], fields[5])
+                    if bed_line in result_dict:
+                        result_dict[bed_line].append(region)
+                    else:
+                        result_dict[bed_line] = [region]
+    return result_dict
+
+def check_files_for_strings(file_data, tra_genes, tran, trah, transposase, cargo):
+    # Ensure strings are in lowercase for case-insensitive comparison
+    lowercase_tra_genes = [s.lower() for s in tra_genes]
+    lowercase_tran = [s.lower() for s in tran]
+    lowercase_trah = [s.lower() for s in trah]
+    lowercase_transposase = [s.lower() for s in transposase]
+    lowercase_cargo = [s.lower() for s in cargo]
+    
+    # Define the specific string and the required count
+    specific_string = 'complete trad'.lower()
+    specific_count_required = 2
+
+    passed_regions = []
+    
+    for file_key, file_content in file_data.items():
+        # Initialize counters for each string for this region
+        tra_genes_counts = {s: 0 for s in lowercase_tra_genes}
+        tran_counts = {s: 0 for s in lowercase_tran}
+        trah_counts = {s: 0 for s in lowercase_trah}
+        transposase_counts = {s: 0 for s in lowercase_transposase}
+        cargo_counts = {s: 0 for s in lowercase_cargo}
+        
+        specific_string_count = 0
+
+        # Check for matches in the file content
+        for entry in file_content:
+            line_lower = ' '.join(map(str.lower, entry))
+            for s in lowercase_tra_genes:
+                if s in line_lower:
+                    tra_genes_counts[s] += 1
+                    if s == specific_string:
+                        specific_string_count += 1
+            for s in lowercase_tran:
+                if s in line_lower:
+                    tran_counts[s] += 1
+            for s in lowercase_trah:
+                if s in line_lower:
+                    trah_counts[s] += 1
+            for s in lowercase_transposase:
+                if s in line_lower:
+                    transposase_counts[s] += 1
+            for s in lowercase_cargo:
+                if s in line_lower:
+                    cargo_counts[s] += 1
+        
+        # Check if all tra_genes strings are present at least once
+        all_tra_genes_present = all(count > 0 for count in tra_genes_counts.values())
+        
+        # Check if at least one string is present in additional lists
+        tran_present = any(count > 0 for count in tran_counts.values())
+        trah_present = any(count > 0 for count in trah_counts.values())
+        transposase_present = any(count > 0 for count in transposase_counts.values())
+        cargo_present = any(count > 0 for count in cargo_counts.values())
+
+        # Additional check for specific string count
+        if all_tra_genes_present and tran_present and trah_present and transposase_present and cargo_present and specific_string_count >= specific_count_required:
+            passed_regions.append(file_key)
+    
+    return passed_regions
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process BED and TXT files.")
+    parser.add_argument("bed_file", help="Input BED file")
+    parser.add_argument("txt_file", help="Input TXT file")
+    args = parser.parse_args()
+
+    # Parse bed file
+    bed_dict = parse_bed_file(args.bed_file)
+    
+    # Parse txt file
+    result_dict = parse_txt_file(args.txt_file, bed_dict)
+    
+    # Example usage with dictionary data
+    tra_genes = ['complete traa', 'complete trab', 'complete trac', 'complete trad', 'complete trae', 'complete traf', 'complete trag', 'complete trai', 'complete trak', 'complete tral', 'complete tran', 'complete trau', 'complete trav', 'complete traw', 'complete integrase']
+    tran = ['complete tran\t', 'complete tran '] # Not contained in tra_genes as there are mishits otherwise
+    trah = ['complete trah\t', 'complete trah '] # Not contained in tra_genes as there are mishits otherwise
+    transposase = ['transposase', 'tpn', 'is630', 'is110', 'is5', 'isot6']
+    cargo = ['spot', 'ppGpp', 'synthetase', 'hydrolase', 'methyltransferase', 'helicase', 'histidine kinase', 'mrp', 'atp-binding', 'endonuclease', 'hnh', 'membrane protein', 'ankyrin', 'ank', 'tetratricopeptide', 'tpr', 'hypothetical'] # Initially just going to take the proteins from Jeanne's paper - may add more from the list_combined file in the future
+    
+    passed_regions = check_files_for_strings(result_dict, tra_genes, tran, trah, transposase, cargo)
+    
+    # Extract base name of the TXT file and append "_complete_RAGEs.bed" to it
+    output_file_name = os.path.splitext(os.path.basename(args.txt_file))[0] + "_complete_RAGEs.bed"
+    
+    # Write passed regions to the output file
+    with open(output_file_name, "w") as bed_output:
+        for region in passed_regions:
+            bed_output.write(region + "\n")
+
+# This currently doesn't require complete integrases and it probably should - need to investigate this.
