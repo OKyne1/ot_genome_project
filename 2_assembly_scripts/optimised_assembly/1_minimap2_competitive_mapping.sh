93a3c979034fdfc1c635b8976be2e244599992cb
OKyne1 <oakemkyne@gmail.com>
2024-07-03 09:58:47 +0700

    Update 1_minimap2_competitive_mapping.sh

--- 2_assembly_scripts/optimised_assembly/1_minimap2_competitive_mapping.sh
+++ 2_assembly_scripts/optimised_assembly/1_minimap2_competitive_mapping.sh
@@ -14,13 +14,17 @@ echo "OS=$(uname -s)"
 echo "username=$(whoami)"
 echo "Usage: $0 $@"
 
+#################################################### Mouse removal code #################################################################################################
+# This code filters out reads that map to the mouse genome and not the karp genome. Future use of this code may want to change the reference doc to include more Ot diversity and also get reads mapped to all of these different genomes.
+# Lines 2-15 are specific to the slurm computing cluster (BMRC cluster). If running locally, remove these, threading may need to be changed in some of the lines.
+# The environment activation stuff will need to be modified for the user, minimap2 and samtools should be present in the environment.
+# Genomes used for this competitive mapping is found in ./genomes_for_mapping/ relative to this script and contains karp, mouse genome and the dilute control sequence (positive control)
+#########################################################################################################################################################################
+
+# conda environment activation
 source /well/moru-batty/users/vhs789/miniforge/etc/profile.d/conda.sh
 conda activate minimap
 
-# This code filters out reads that map to the mouse genome and not the karp genome. Future use of this code will need to change the reference doc to include more Ot diversity and also get reads mapped to all of these different genomes.
-
-# Ensure that minimap2 and samtools are available in the environment (env: minimap2)
-# Check the names of the "chromosomes" AKA bacterial genomes and modify as required.
 
 # Check for the correct number of command-line arguments
 if [ "$#" -ne 2 ]; then
@@ -31,34 +35,29 @@ fi
 reference="$1"
 reads="$2"
 
-# minimap2 mapping
+# minimap2 mapping of reads against the combined reference
 minimap2 -x map-ont -t 6 -a -o aligned_reads.sam "$reference" "$reads"
 
 # Convert SAM to BAM
 samtools view -@ 4 -bS aligned_reads.sam > aligned_reads.bam
-
 echo "alignment complete"
 
 # Sort BAM file
 samtools sort -@ 4 -o aligned_reads_sorted.bam aligned_reads.bam
-
 echo "sorting complete"
 
 # Index the sorted BAM file
 samtools index aligned_reads_sorted.bam
-
 echo "indexing complete"
 
 # Extract unmapped reads
 samtools view -f 4 -b aligned_reads_sorted.bam | samtools bam2fq - > unmapped.fastq
 
-# This should allow us to view the chromosomes present.
-#samtools view -H aligned_reads_sorted.bam # enables identification of the different chromosomes present. These are the things following SN:
+# This code produces the Ot aligned reads (based on the name of the karp chromosome: LS398548.1).  
+# If the file/genomes are modified then this may need to be changed.
+samtools view -b -@ 4 aligned_reads_sorted.bam LS398548.1 > mapped_ot_reads.bam
 
-# This code produces the Ot aligned reads. Change the chromosome name as appropriate
-#samtools view -b -@ 4 aligned_reads_sorted.bam LS398548.1 > mapped_ot_reads.bam
-
-# Filter out unmapped reads before extracting mapped reads
+# Filter out unmapped reads before extracting LS398548.1 mapped reads
 samtools view -F 4 -b -@ 4 aligned_reads_sorted.bam LS398548.1 > mapped_ot_reads.bam
 
 # Convert to a fastq format
@@ -66,5 +65,4 @@ samtools bam2fq mapped_ot_reads.bam > mapped_ot_reads.fastq
 
 # merge the fastq files
 cat unmapped.fastq mapped_ot_reads.fastq > ot_and_unmapped.fastq
-
 echo "mapping complete"
